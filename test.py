1d62d972205acebbae06e8aac22cf2aab6b2c5d0
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-04-17 22:53:50 +0530

    learnt about queues, its 3 types (fifo, lifo and priority). they're special because queues are thread-safe out of the box. Also read about lists. Noted that only the append() method in lists is inherently thread-safe. Also learnt a lot lot about immutables, cpython, bytecode and how python (cpython in particular) gets executed.

--- test.py
+++ test.py
@@ -1,34 +1,32 @@
-from threading import Lock
+import threading
+from queue import PriorityQueue
+import time
 
-def lock_class(methodnames, lockfactory):
-    return lambda cls: make_threadsafe(cls, methodnames, lockfactory)
+def mySubscriber(queue: PriorityQueue):
+    while not queue.empty():
+        item = queue.get()
+        if item is None:
+            break
+        print("{} removed {} from the queue".format(threading.current_thread(), item))
+        queue.task_done()
+        time.sleep(1)
 
-def lock_method(method):
-    if getattr(method, '__is_locked', False):
-        raise TypeError("Method %r is already locked!" %method)
+myQueue = PriorityQueue()
 
-    def locked_method(self, *arg, **kwarg):
-        with self._lock:
-            return method(self, *arg, **kwarg)
+for i in range(5):
+    myQueue.put(i, i)
 
-    locked_method.__name__ = '%s(%s)' % ('lock_method', method.__name__)
-    locked_method.__is_locked = True
-    return locked_method
+for i in range(5):
+    myQueue.put(i, i)
 
-def make_threadsafe(cls, methodnames, lockfactory):
-    init = cls.__init__
-    def newinit(self, *arg, **kwarg):
-        init(self, *arg, **kwarg)
-        self._lock = lockfactory()
-    cls.__init__ = newinit
-    for methodname in methodnames:
-        oldmethod = getattr(cls, methodname)
-        newmethod = lock_method(oldmethod)
-        setattr(cls, methodname, newmethod)
-    return cls
+print("Queue Populated")
+threads = []
+for i in range(2):
+    thread = threading.Thread(target=mySubscriber, args=(myQueue,))
+    thread.start()
+    threads.append(thread)
 
-@lock_class(['add','remove'], Lock)
-class ClassDecoratorLockedSet(set):
-    @lock_method  # if you double-lock a method, a TypeError is raised
-    def lockedMethod(self):
-        print("This section of our code would be thread safe")
+for thread in threads:
+    thread.join()
+    
+print("Queue is empty")
