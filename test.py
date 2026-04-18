1e9e7b33c245d32b98679a5ffc8fe7b0bfdaff21
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-04-18 12:32:52 +0530

    learnt how to work with deques

--- test.py
+++ test.py
@@ -1,32 +1,7 @@
-import threading
-from queue import PriorityQueue
-import time
-
-def mySubscriber(queue: PriorityQueue):
-    while not queue.empty():
-        item = queue.get()
-        if item is None:
-            break
-        print("{} removed {} from the queue".format(threading.current_thread(), item))
-        queue.task_done()
-        time.sleep(1)
-
-myQueue = PriorityQueue()
-
-for i in range(5):
-    myQueue.put(i, i)
-
-for i in range(5):
-    myQueue.put(i, i)
-
-print("Queue Populated")
-threads = []
-for i in range(2):
-    thread = threading.Thread(target=mySubscriber, args=(myQueue,))
-    thread.start()
-    threads.append(thread)
-
-for thread in threads:
-    thread.join()
-    
-print("Queue is empty")
+import collections
+dq = collections.deque('123456')
+print("Deque: {}".format(dq))
+dq.rotate(3) # shift right
+print("Deque: {}".format(dq))
+dq.rotate(-2) # shift left
+print("Deque {}".format(dq))
