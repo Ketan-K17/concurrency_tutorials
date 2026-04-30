75593ef76d014dcde2062000815149d601feb039
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-04-30 15:38:18 +0530

    Futures: Learnt about concurrent futures, the threadpoolexecutor and processpoolexecutor classes, and how to use them to write easier-to-read multithreaded / process applications.

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 import threading
 from crawler import *
 from checkablequeue import *
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import csv
 
 THREAD_COUNT = 20
 linksToCrawl = CheckableQueue()
 
-def createCrawlers(): # our thread factory
-    for i in range(THREAD_COUNT):
-        t = threading.Thread(target=consume_link, daemon=True)
-        t.start()
+# def createCrawlers(): # our thread factory
+#     for i in range(THREAD_COUNT):
+#         t = threading.Thread(target=consume_link, daemon=True)
+#         t.start()
 
-def consume_link():
-    while True:
-        url = linksToCrawl.get()
-        try:
-            if url is None:
-                break
-            Crawler.crawl(threading.current_thread(), url, linksToCrawl)
-        except:
-            print("Exception")
-        finally:
-            linksToCrawl.task_done()
+def appendToCSV(result):
+    print("Appending result to CSV File: {}".format(result))
+    with open('results.csv', 'a') as csvfile:
+        resultwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        resultwriter.writerow(result)
+
+def consume_link(url):
+    try:
+        Crawler.crawl(threading.current_thread(), url, linksToCrawl)
+    except:
+        print("Exception thrown with link: {}".format(url))
+    finally:
+        linksToCrawl.task_done()
     
 def main():
     url = input("Website > ")
     Crawler(url)
     linksToCrawl.put(url)
-    createCrawlers()
-    linksToCrawl.join() # waiting for there to be NO un-explored links in the queue => tasks have finished, we can proceed
+    with ThreadPoolExecutor(max_workers=THREAD_COUNT) as executor:
+        futures = []
+        while True:
+            try:
+                link = linksToCrawl.get(timeout=1)
+                if link is not None:
+                    future = executor.submit(consume_link, link)
+                    futures.append(future)
+            except:
+                if linksToCrawl.empty():
+                    break
+
+        for future in as_completed(futures):
+            try:
+                if future.result() != None:
+                    appendToCSV(future.result())
+            except:
+                print(future.exception())
+
     print("Total Links Crawled: {}".format(len(Crawler.crawledLinks)))
     print("Total Errors: {}".format(len(Crawler.errorLinks)))
 
--- test.py
+++ test.py
@@ -1,36 +1,39 @@
-from urllib.request import Request, urlopen, urljoin, URLError
-from urllib.parse import urlparse
-import ssl
-from bs4 import BeautifulSoup
-class Crawler:
-    base_url = ''
-    myssl = ssl.create_default_context()
-    myssl.check_hostname=False
-    myssl.verify_mode=ssl.CERT_NONE
-    errorLinks = set
+import timeit
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+import math
 
-    def __init__(self, base_url):
-        Crawler.base_url = base_url
+PRIMES = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419
+]
 
-    @staticmethod
-    def crawl(thread_name, url, linksToCrawl):
-        try:
-            link = urljoin(Crawler.base_url, url)
+def is_prime(n):
+    if n % 2 == 0:
+        return False
 
-            if (urlparse(link).netloc == 'tutorialedge.net') and (link not in Crawler.crawledLinks):
-                request = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-                response = urlopen(request, context=Crawler.myssl)
-                Crawler.crawledLinks.add(link)
-                print("Url {} Crawled with Status: {} : {} Crawled In Total".format(response.geturl(), response.getcode(), len(Crawler.crawledLinks)))
-                soup = BeautifulSoup(response.read(), "html.parser")
-                Crawler.enqueueLinks(soup.find_all('a'), linksToCrawl)
-        except URLError as e:
-            print("URL {} threw this error when trying to parse: {}".format(link, e.reason))
-            Crawler.errorLinks.add(link)
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
-    @staticmethod
-    def enqueueLinks(links, linksToCrawl):
-        for link in links:
-            if (urljoin(Crawler.base_url, link.get('href')) not in Crawler.crawledLinks):
-                if (urljoin(Crawler.base_url, link.get('href')) not in linksToCrawl):
-                    linksToCrawl.put(link.get('href'))
+def main():
+    t1 = timeit.default_timer()
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+    print("{} Seconds Needed for ProcessPoolExecutor".format(timeit.default_timer() - t1))
+
+    t2 = timeit.default_timer()
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+    print("{} Seconds Needed for ThreadPoolExecutor".format(timeit.default_timer() - t2))
+
+if __name__ == '__main__':
+    main()
